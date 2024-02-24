6057b4ad63a3330277ed20aa2416c8ac31941d43
Satish D <sdhamodhara@apple.com>
2024-02-24 11:36:29 -0600

    initial code push

---
+++ src/main/java/com/function/HelloCloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCloudFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/function/HelloCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
